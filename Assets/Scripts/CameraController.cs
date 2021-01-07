5da05c67e1af97b489f3ca1d60ad44b6b4e0a8ed
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-07 09:46:37 -0500

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 newPos = target.position + offset;
+        newPos.z = transform.position.z;
+        transform.position = newPos;
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarController : MonoBehaviour
+{
+    bool move = false;
+    bool isGrounded = false;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 20f;
+    [SerializeField] float rotationSpeed = 7f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            move = true;
+        if (Input.GetButtonUp("Fire1"))
+            move = false;
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void FixedUpdate()
+    {
+        if (move == true)
+            if (isGrounded)
+                rb.AddForce(transform.right * speed * Time.deltaTime * 100f, ForceMode2D.Force); // ForceMode2D.Force is the default anyway
+            else
+                rb.AddTorque(rotationSpeed * Time.deltaTime * 100f, ForceMode2D.Force);
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D()
+    {
+        isGrounded = false;
+    }
+}
